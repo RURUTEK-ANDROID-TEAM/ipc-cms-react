5cbc3fb1c2029140e9c7e0555042c5e81b981407
KISHORE-KUMAR-S <livekishore2001@gmail.com>
2025-08-23 18:10:46 +0530

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/home";
+import { Button } from "@/components/ui/button";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <div className="flex min-h-svh flex-col items-center justify-center">
+    <Button>Click me</Button>
+  </div>;
+}
